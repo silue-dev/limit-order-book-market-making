63e64e874aa180a9d0a0388d52f6345b90dc752d
Bram Silue <bram@silue.dev>
2023-12-29 00:13:51 +0100

    added core method blueprint definitions

--- src/orderbook.py
+++ src/orderbook.py
@@ -1,6 +1,18 @@
+from collections import deque
 from orders import OrderTree
 
 class OrderBook:
     def __init__(self):
         self.bids = OrderTree()
         self.asks = OrderTree()
+        self.tape = deque()
+        self.time = 0
+    
+    def add_limit_order():
+        pass
+    
+    def add_market_order():
+        pass
+    
+    def del_order():
+        pass
