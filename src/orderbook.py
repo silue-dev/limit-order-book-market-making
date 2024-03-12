6fe3e0068372161b70f34d327bfa0a5f3437bd6a
Bram Silue <bram@silue.dev>
2024-03-12 22:31:35 +0100

    adjusted basic blueprint

--- src/orderbook.py
+++ src/orderbook.py
@@ -10,22 +10,34 @@ class OrderBook:
         self.bids = OrderTree()
         self.asks = OrderTree()
         self.tape = deque()
-        self.time = 0
+        self.order_num = 0
     
-    def add_limit_order():
+    def add_order(self, order):
+        if order.type == 'market':
+            self.add_market_order(order)
+        elif order.type == 'limit':
+            self.add_limit_order(order)
+        elif order.type == 'ioc':
+            self.add_ioc_order(order)
+    
+    def add_market_order(self, order):
         pass
-    
-    def add_market_order():
+
+    def add_limit_order(self, order):
         pass
-    
+
+    def add_ioc_order(self, order):
+        pass
+
     def del_order():
         pass
 
-    def cancel_order():
-        pass
+    def get_best_bid(self):
+        return self.bids.max_price()
 
-    def get_best_bid():
-        pass
-
-    def get_best_ask():
-        pass
+    def get_best_ask(self):
+        return self.asks.min_price()
+    
+    def update_order_num(self):
+        self.order_num += 1
+        return self.order_num
